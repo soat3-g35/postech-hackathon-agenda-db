0447d78d5528edda22975f8bac4dc0dcf31ec90b
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-07-22 08:33:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,17 +23,29 @@ output "subnet_cidr_blocks" {
   value = [for s in data.aws_subnet.example : s.cidr_block]
 }
 
+resource "aws_security_group" "instance" {
+  name   = "postgres-security-group"
+  vpc_id = data.aws_vpc.selected.id
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_db_instance" "paciente" {
-  identifier          = "paciente"
-  name                = "paciente"
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 5
-  engine              = "postgres"
-  engine_version      = "14.11"
-  username            = "postgres"
-  password            = "postgres"
-  publicly_accessible = true
-  skip_final_snapshot = true
+  identifier             = "paciente"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  name                   = "paciente"
+  engine                 = "postgres"
+  engine_version         = "14.11"
+  username               = "postgres"
+  password               = "postgres"
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
     Name = "PacientePostgresDB"
