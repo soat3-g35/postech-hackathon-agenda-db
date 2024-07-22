b67e0578a827215319eefeee10a2103e2ef906ce
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-07-22 20:10:18 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,36 +14,46 @@ data "aws_subnet_ids" "example" {
   vpc_id = data.aws_vpc.selected.id
 }
 
-data "aws_security_group" "selected" {
+data "aws_subnet" "example" {
+  for_each = data.aws_subnet_ids.example.ids
+  id       = each.value
+}
+
+output "subnet_cidr_blocks" {
+  value = [for s in data.aws_subnet.example : s.cidr_block]
+}
+
+data "aws_security_group" "sg" {
+  vpc_id = data.aws_vpc.selected.id
   filter {
     name   = "tag:Name"
-    values = ["postech-vpc"]
+    values = ["g35-eks-cluster-cluster"]
   }
 }
 
-resource "aws_db_subnet_group" "paciente" {
-  name       = "paciente"
+resource "aws_db_subnet_group" "pedido" {
+  name       = "pedido"
   subnet_ids = data.aws_subnet_ids.example.ids
 
   tags = {
-    Name = "postech-vpc"
+    Name = "Pedido"
   }
 }
 
-resource "aws_db_instance" "paciente" {
-  identifier          = "paciente"
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 5
-  engine              = "postgres"
-  engine_version      = "14.11"
-  username            = "postgres"
-  password            = "postgres"
-  publicly_accessible = true
-  skip_final_snapshot = true
-
-  vpc_security_group_ids = [data.aws_security_group.selected.id]
-  db_subnet_group_name   = aws_db_subnet_group.paciente.name
+resource "aws_db_instance" "pedido" {
+  identifier             = "pedido"
+  name                   = "pedido"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.11"
+  username               = "postgres"
+  password               = "postgres"
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.pedido.name
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
-    Name = "PacientePostgresDB"
+    Name = "PedidoPostgresDB"
   }
 }
