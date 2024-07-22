30ba118fc0c25221ee4eba603389d1c02dd52b98
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-07-22 08:23:28 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,8 +34,6 @@ resource "aws_db_instance" "paciente" {
   password               = "postgres"
   publicly_accessible    = true
   skip_final_snapshot    = true
-  db_subnet_group_name   = aws_db_subnet_group.paciente.name
-  vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
     Name = "pacientePostgresDB"
   }
