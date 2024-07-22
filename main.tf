05c230334347bac795326c2ed9c3fe36aba7111b
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-07-22 19:41:49 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,40 +10,39 @@ data "aws_vpc" "selected" {
   }
 }
 
-data "aws_subnet_ids" "example" {
+data "aws_subnet" "selected" {
+  filter {
+    name   = "tag:kubernetes.io/cluster/education-eks-cluster"
+    values = ["shared"]
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name   = "postgres-security-group"
   vpc_id = data.aws_vpc.selected.id
-}
-
-data "aws_subnet" "example" {
-  for_each = data.aws_subnet_ids.example.ids
-  id       = each.value
-}
-
-output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.example : s.cidr_block]
-}
-
-resource "aws_db_subnet_group" "paciente" {
-  name       = "paciente"
-  subnet_ids = data.aws_subnet_ids.example.ids
-
-  tags = {
-    Name = "Paciente"
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_db_instance" "paciente" {
-  identifier           = "paciente"
-  instance_class       = "db.t3.micro"
-  allocated_storage    = 5
-  name                 = "paciente"
-  engine               = "postgres"
-  engine_version       = "14.11"
-  username             = "postgres"
-  password             = "postgres"
-  publicly_accessible  = true
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.paciente.name
+  identifier          = "paciente"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 5
+  engine              = "postgres"
+  engine_version      = "14.11"
+  username            = "postgres"
+  password            = "postgres"
+  publicly_accessible = true
+  skip_final_snapshot = true
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  # subnet_id              = data.aws_subnet.selected.id
+
+  db_name = "paciente"
 
   tags = {
     Name = "PacientePostgresDB"
